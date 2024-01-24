7f301d2c7f5d73db1bb4b69af2a533899d57ee65
Michaiah Bos <michaiahbos@yahoo.com>
2024-01-24 11:13:33 -0600

    wip

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,25 +1,32 @@
-import React, {useEffect} from "react"
-import { useNavigate } from "react-router-dom"
-import axios from "axios"
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Logout = () => {
+  const navigate = useNavigate();
 
-    useEffect(()=> {
-        const token = localStorage.getItem('token');
-        const navigate = useNavigate();
- axios.post("http://localhost:9000/api/logout", {}, {
-    headers: {
-        authorization: token
-    }
- })
- .then(resp => {
-    localStorage.removeItem('token')
-    navigate('/login')
- })
- .catch(err => {
-    console.log(err)
- })
-    },[])
-    return(<div></div>)
-}
-export default Logout
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .post(
+        "http://localhost:9000/api/logout",
+        {},
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      )
+      .then((resp) => {
+        localStorage.removeItem("token");
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return <div></div>;
+};
+
+export default Logout;
