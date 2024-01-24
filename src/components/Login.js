6d24d17ee9c9f09017d464ddbf3f1b80cdadc326
Michaiah Bos <michaiahbos@yahoo.com>
2024-01-23 21:00:02 -0600

    working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
+   const navigate = useNavigate();
+    const [cred, setCred] = useState({
+        username: '',
+        password: ''
+    });
+    const handleChange = (e) => {
+     setCred({
+        ...cred,
+        [e.target.name]: e.target.value
+     })
+    }
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      axios.post('http://localhost:9000/api/login', cred )
+      .then(resp => {
+        localStorage.setItem("token", resp.data.payload);
+        navigate('/friends')
+
+      })
+      .catch(err => {
+        console.log(err.resp);
+      })
+
+    }
+
   return (
     <div>
       <h1>Login</h1>
-
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username: 😊 </label>
-          <input id="username" />
+          <input onChange={handleChange} name='username' id="username" />
         </div>
         <div>
           <label htmlFor="password">Password: 🔑</label>
-          <input type="password" id="password" />
+          <input onChange={handleChange} name='password' type="password" id="password" />
         </div>
         <button>Submit</button>
       </form>
