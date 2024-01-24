9d154f341d8c9b7801784700f69bdd2a46f88b37
Michaiah Bos <michaiahbos@yahoo.com>
2024-01-23 18:36:12 -0600

    updated so routes work

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 const Login = () => {
   return (<h2>Login</h2>)
 }
 const FriendsList = () => {
-  return (<h2>Login</h2>)
+  return (<h2>FriendList</h2>)
 };
 const AddFriend = () => {
-  return (<h2>Login</h2>)
+  return (<h2>AddFriend</h2>)
 }
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/">
-          <Login />
-        </Route>
-        <Route path="/friends">
-          <FriendList />
-        </Route>
-        <Route path="/friends/add">
-          <AddFriend />
-        </Route>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/friends" element={<FriendsList />} />
+          <Route path="/friends/add" element={<AddFriend />} />
+        </Routes>
       </div>
     </Router>
   );
